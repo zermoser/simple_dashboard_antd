87a7b2b5206114a5cad107ded9a4a8c4020317eb
zermoser <kittipoj.na@gmail.com>
2024-07-03 13:06:02 +0700

    Deploy !

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/simple_dashboard_antd/',
   plugins: [react()],
   server: {
     port: 3003
